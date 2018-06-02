2d22a0fd0942ad44174001918866868e2c7a7818
Robert Su <robsu@microsoft.com>
2018-06-02 14:25:23 -0700

    clean up header's styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   @Output() menuToggled = new EventEmitter<any>();
 
-  constructor() {
-  }
-
-
-  ngOnInit() {
-  }
-
+  /* Emits a signle indicating the hamburger menu has been toggled */
   toggleMenu() {
     this.menuToggled.emit();
+  }
+
+  /**
+   * Returns the proper logo image src based on the support of WebP technology
+   */
+  logoImgSrc() {
+    const supportWebP = typeof window === 'undefined' || (<any>window).supportsWebP;
+    if (supportWebP) {
+      return 'assets/index_name.webp';
+    } else {
+      return 'assets/index_name.png';
+    }
   }
 }

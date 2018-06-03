edae1bff27c8da2f97f4f2d46b5d3122381a3b1a
Robert Su <robsu@microsoft.com>
2018-06-02 20:44:47 -0700

    correct contact page's styles

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,44 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+	selector: 'app-contact',
+	templateUrl: './contact.component.html',
+	styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+	constructor(private meta: Meta, private title: Title) {}
 
-  constructor(
-    private meta: Meta,
-    private title: Title) {
-    this.setMetaTags();
-  }
+	ngOnInit() {
+		this.setMetaTags();
+	}
 
-  private setMetaTags() {
-    // sets the title.
-    this.title.setTitle('Emily Chen (陳逸芸) | Graphic Designer');
+	/** Set all the meta tags */
+	private setMetaTags() {
+		// sets the title.
+		this.title.setTitle('Emily Chen (陳逸芸) | Graphic Designer');
 
-    // sets the description tag
-    this.meta.updateTag({
-      content: 'Graphic designer based in Taiwan. Focus on graphic design, brand identity design, art and cultural performance.'
-    },
-      "property='og:description'"
-    )
+		// sets the description tag
+		this.meta.updateTag(
+			{
+				content:
+					'Graphic designer based in Taiwan. Focus on graphic design, brand identity design, art and cultural performance.'
+			},
+			"property='og:description'"
+		);
 
-    // sets thumbnail.
-    this.meta.updateTag({
-      content: 'http://www.emilygph.com/assets/emily.png'
-    },
-      "property='og:image'"
-    )
+		// sets thumbnail.
+		this.meta.updateTag(
+			{
+				content: 'http://www.emilygph.com/assets/emily.png'
+			},
+			"property='og:image'"
+		);
 
-    this.meta.updateTag({
-      content: 'http://www.emilygph.com/assets/emily.png'
-    },
-      "property='og:image:url'"
-    )
-  }
-
-  ngOnInit() {
-  }
-
+		this.meta.updateTag(
+			{
+				content: 'http://www.emilygph.com/assets/emily.png'
+			},
+			"property='og:image:url'"
+		);
+	}
 }
